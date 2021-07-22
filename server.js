4057a4af82d63363d41a19199719ed7bd95adf31
Emilly sandy <38957417+emillysandy@users.noreply.github.com>
2021-07-22 14:10:35 -0300

    Deletando alguns comentários

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-//importando o express para o documento
+
 const { response } = require("express")
 
 const express = require("express")
 
-//intanciando o express
 const app = express()
 
-//Importando arquivo JSON de estados
 const estados = require("./data/estados-cidades.json")
 
 app.get("/estados/todos", (request, response)=>{
@@ -17,7 +15,7 @@ app.get("/estados/:sigla", (request, response) => {
     response.status(200).send(estados.find(estado => estado.sigla == siglaRequisitada))
 })
 
-//Definindo uma porta para o servidor local através de uma função anônima
+
 app.listen(8000, () => {
     console.log("Servidor rodando na porta 8000")
 })
